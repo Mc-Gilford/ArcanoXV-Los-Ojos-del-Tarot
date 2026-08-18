15a72094f77e0caf5af189b5cd46712918632c8f
McGilford <34671406+Mc-Gilford@users.noreply.github.com>
2026-08-18 10:50:28 -0600

    Addiing the camera effects

--- Assets/Scripts/Camera/CameraColor.cs
+++ Assets/Scripts/Camera/CameraColor.cs
@@ -6,9 +6,14 @@ public class CameraColor : MonoBehaviour
 {
     private Volume globalVolume;
     private ColorAdjustments colorAdjustments;
+    private Vignette vignette;
 
     private Color originalColor = Color.white;
-    private Color redColor = Color.red;
+    private Color redColor = new Color(1f, 0.55f, 0.55f);
+
+    private float originalVignetteIntensity;
+    private float originalVignetteSmoothness;
+    private Color originalVignetteColor;
 
     private Jugador playerScript;
     private GameObject player;
@@ -24,119 +29,111 @@ public class CameraColor : MonoBehaviour
         if (player != null)
         {
             playerScript = player.GetComponent<Jugador>();
-
-            Debug.Log(
-                "Jugador script encontrado: " +
-                (playerScript != null)
-            );
+            Debug.Log("Jugador script encontrado: " + (playerScript != null));
         }
 
         globalVolume = GetComponent<Volume>();
 
-        Debug.Log(
-            "Volume encontrado: " +
-            (globalVolume != null)
-        );
+        Debug.Log("Volume encontrado: " + (globalVolume != null));
 
         if (globalVolume != null)
         {
-            Debug.Log(
-                "Volume Profile: " +
-                globalVolume.profile
-            );
+            Debug.Log("Volume Profile: " + globalVolume.profile);
 
-            bool colorFound =
-                globalVolume.profile.TryGet(out colorAdjustments);
+            // Buscar Color Adjustments
+            bool colorFound = globalVolume.profile.TryGet(out colorAdjustments);
 
-            Debug.Log(
-                "ColorAdjustments encontrado: " +
-                colorFound
-            );
+            Debug.Log("ColorAdjustments encontrado: " + colorFound);
 
-            Debug.Log(
-                "ColorAdjustments null: " +
-                (colorAdjustments == null)
-            );
+            // Buscar Vignette
+            bool vignetteFound = globalVolume.profile.TryGet(out vignette);
+
+            Debug.Log("Vignette encontrado: " + vignetteFound);
 
             if (colorAdjustments != null)
             {
                 colorAdjustments.colorFilter.overrideState = true;
 
-                originalColor =
-                    colorAdjustments.colorFilter.value;
+                originalColor = colorAdjustments.colorFilter.value;
 
-                Debug.Log(
-                    "Color original: " +
-                    originalColor
-                );
+                Debug.Log("Color original: " + originalColor);
+            }
 
-                Debug.Log(
-                    "Override activo: " +
-                    colorAdjustments.colorFilter.overrideState
-                );
+            if (vignette != null)
+            {
+                vignette.intensity.overrideState = true;
+                vignette.smoothness.overrideState = true;
+                vignette.color.overrideState = true;
+
+                originalVignetteIntensity = vignette.intensity.value;
+                originalVignetteSmoothness = vignette.smoothness.value;
+                originalVignetteColor = vignette.color.value;
+
+                Debug.Log("Vignette original intensity: " + originalVignetteIntensity);
             }
         }
     }
 
     void Update()
     {
-        if (playerScript != null && colorAdjustments != null)
+        if (playerScript != null && colorAdjustments != null && vignette != null)
         {
-            float corduraPlayer =
-                playerScript.getCordura();
+            float corduraPlayer = playerScript.getCordura();
 
-            Debug.Log(
-                "Cordura: " +
-                corduraPlayer +
-                " | isRed: " +
-                isRed
-            );
+            Debug.Log("Cordura: " + corduraPlayer + " | isRed: " + isRed);
 
-            if (corduraPlayer <= 5)
+            if (corduraPlayer <= 3 && !isRed)
             {
                 ChangeCameraToRed();
             }
-            else
+            else if (corduraPlayer > 3 && isRed)
             {
                 RestoreCameraColor();
             }
         }
         else
         {
-            Debug.LogWarning(
-                "NO SE PUEDE CAMBIAR COLOR | playerScript: " +
-                (playerScript != null) +
-                " | colorAdjustments: " +
-                (colorAdjustments != null)
-            );
+            Debug.LogWarning("NO SE PUEDE CAMBIAR EFECTO | Player: " + (playerScript != null) + " | ColorAdjustments: " + (colorAdjustments != null) + " | Vignette: " + (vignette != null));
         }
     }
 
     private void ChangeCameraToRed()
     {
+        // Color rojo
         colorAdjustments.colorFilter.overrideState = true;
-
         colorAdjustments.colorFilter.value = redColor;
+
+        // Vignette
+        vignette.intensity.overrideState = true;
+        vignette.smoothness.overrideState = true;
+        vignette.color.overrideState = true;
+
+        vignette.intensity.value = 0.45f;
+        vignette.smoothness.value = 0.7f;
+        vignette.color.value = new Color(0.25f, 0f, 0f);
 
         isRed = true;
 
-        Debug.Log(
-            "APLICANDO ROJO | Color actual: " +
-            colorAdjustments.colorFilter.value
-        );
+        Debug.Log("APLICANDO EFECTO DE CORDURA | Color: " + colorAdjustments.colorFilter.value + " | Vignette: " + vignette.intensity.value);
     }
 
     private void RestoreCameraColor()
     {
+        // Restaurar Color
         colorAdjustments.colorFilter.overrideState = true;
-
         colorAdjustments.colorFilter.value = originalColor;
+
+        // Restaurar Vignette
+        vignette.intensity.overrideState = true;
+        vignette.smoothness.overrideState = true;
+        vignette.color.overrideState = true;
+
+        vignette.intensity.value = originalVignetteIntensity;
+        vignette.smoothness.value = originalVignetteSmoothness;
+        vignette.color.value = originalVignetteColor;
 
         isRed = false;
 
-        Debug.Log(
-            "RESTAURANDO COLOR | Color actual: " +
-            colorAdjustments.colorFilter.value
-        );
+        Debug.Log("RESTAURANDO CAMARA | Color: " + colorAdjustments.colorFilter.value + " | Vignette: " + vignette.intensity.value);
     }
 }
