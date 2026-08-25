4f4dec84d65b78e2e91a0b56d913cc71053e1f35
CisRosChristian <ccisnerosr1500@alumno.ipn.mx>
2026-08-25 16:15:25 -0600

    Habitacion de las cajas funcional

---
+++ Assets/Scripts/BoxRoom/Box.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    [SerializeField] private float volume;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if(audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+    
+    void Start()
+    {
+        volume = 0.6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            audioSource.PlayOneShot(audioClip, volume);
+            Destroy(gameObject,0.2f);
+        }
+    }
+}
