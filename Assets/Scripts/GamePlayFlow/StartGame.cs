37397033b2619b20430fad504db7ee61f0748b9f
DanyRAAD <ramirezandresdaniela@gmail.com>
2026-08-25 17:05:49 -0600

    add canvas startgame and tutorial

--- Assets/Scripts/GamePlayFlow/StartGame.cs
+++ Assets/Scripts/GamePlayFlow/StartGame.cs
@@ -3,10 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    [SerializeField] private GameObject tutorialPanel;
+    
     public void StartGameScene()
     {
         // Iniciar con la escena del carro (trayecto hacia la mansión),
         // que luego transiciona sola a PrincipalScene.
-        SceneManager.LoadScene("Carro y salida");
+        SceneManager.LoadScene(2);
+    }
+
+    public void OpenTutorial()
+    {
+        tutorialPanel.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorialPanel.SetActive(false);
     }
 }
